76c3e82d4a6f7c7ffaa6bf8bf7ea92b6331bc08f
Dmitr-The-II <74722555+Dmitr-The-II@users.noreply.github.com>
2021-04-16 18:23:42 +0300

    Add files via upload

--- final-question3/main.c
+++ final-question3/main.c
@@ -20,12 +20,41 @@
 // Main: Entry-point to the application
 // -----------------------------------------------------------------------------------
 int main(void) {
+	// sum of mins screen time and num of bytes for 4 people. 
+// [0] - Billy Bob
+// [1] - Sue Ellen
+// [2] - Jimmy Joe
+// [3] - Sally Sue
+	double mins[3], screenTime[3];
+	int bytes[3], choice;
 	struct Phone phone[20] = { {0} };
 	Main_menu(phone);
+	for (int i = 0; i < 7; i++) {
+		if (phone[i].name == "Billy Bob") {
+			mins[0] += phone[i].callMinutes;
+			screenTime[0] += phone[i].screenTime;
+			bytes[0] += phone[i].dataBytes;
+		}
+		else if (phone[i].name == "Sue Ellen") {
+			mins[1] += phone[i].callMinutes;
+			screenTime[1] += phone[i].screenTime;
+			bytes[1] += phone[i].dataBytes;
+		}
+		else if (phone[i].name == "Jimmy Joe") {
+			mins[2] += phone[i].callMinutes;
+			screenTime[2] += phone[i].screenTime;
+			bytes[2] += phone[i].dataBytes;
+		}
+		else if (phone[i].name == "Sally Sue") {
+			mins[3] += phone[i].callMinutes;
+			screenTime[3] += phone[i].screenTime;
+			bytes[3] += phone[i].dataBytes;
+		}
+	}
 	
 		printf("Select one of the following options :\n0 - Exit\n1 - Show users by call duration\n2 - Show users by data usage\n3 - Show users by screen time\n4 - Show all user data\n");
 		
-		/*while (a == 0) {
+		while (1) {
 			scanf("%d", &choice);
 			switch (choice) {
 			case 1:
@@ -37,13 +66,13 @@ int main(void) {
 			case 4:
 				all_user_data();
 			case 0:
-				a = 0;
+				break;
 			default:
 				printf("ERROR, please try again!\n");
 			}
 
 
-		}*/
+		}
 		printf("\nGoodbye.\n");
 		
 }
